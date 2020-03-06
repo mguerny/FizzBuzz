76fc139d0089e052057300564fddc5e1b39be566
mguerny <marin.guerny@laposte.net>
2020-03-06 11:17:50 +0100

    Green step : 4 tests ok

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,4 +2,13 @@ class FizzBuzz:
 
     @staticmethod
     def fizzbuzz(number):
-        return 'red_step'
+        if number % 3 == 0 and number%5 == 0:
+            return 'fizzbuzz'
+        else:
+            if number % 3 == 0:
+                return 'fizz'
+            else:
+                if number % 5 == 0:
+                    return 'buzz'
+                else:
+                    return ''
