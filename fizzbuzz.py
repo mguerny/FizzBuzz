eaaf79dce370b7d531f162a792c1e6c638e66642
mguerny <marin.guerny@laposte.net>
2020-03-06 11:23:43 +0100

    Blue step : refactor

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,13 +2,7 @@ class FizzBuzz:
 
     @staticmethod
     def fizzbuzz(number):
-        if number % 3 == 0 and number%5 == 0:
-            return 'fizzbuzz'
-        else:
-            if number % 3 == 0:
-                return 'fizz'
-            else:
-                if number % 5 == 0:
-                    return 'buzz'
-                else:
-                    return ''
+        return 'fizzbuzz' if number % 3 == 0 and number % 5 == 0 else \
+            ('fizz' if number % 3 == 0 else
+                ('buzz' if number % 5 == 0 else
+                    ''))
