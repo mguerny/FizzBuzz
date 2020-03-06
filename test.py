b797822b1630edfc5822eb25b9c22ca9d2e9c584
mguerny <marin.guerny@laposte.net>
2020-03-06 11:16:15 +0100

    Oups, forgot the values. 4 tests still fail

--- test.py
+++ test.py
@@ -5,16 +5,16 @@ from fizzbuzz import FizzBuzz
 class TestFizzBuzz(unittest.TestCase):
 
     def test_should_print_nothing_if_not_multiple_of_3_or_5(self):
-        self.assertEqual(FizzBuzz.fizzbuzz(), '')
+        self.assertEqual(FizzBuzz.fizzbuzz(2), '')
 
     def test_should_print_fizz_if_multiple_of_3(self):
-        self.assertEqual(FizzBuzz.fizzbuzz(), 'fizz')
+        self.assertEqual(FizzBuzz.fizzbuzz(3), 'fizz')
 
     def test_should_print_buzz_if_multiple_of_5(self):
-        self.assertEqual(FizzBuzz.fizzbuzz(), 'buzz')
+        self.assertEqual(FizzBuzz.fizzbuzz(5), 'buzz')
 
     def test_should_print_fizzbuzz_if_multiple_of_3_and_5(self):
-        self.assertEqual(FizzBuzz.fizzbuzz(), 'fizzbuzz')
+        self.assertEqual(FizzBuzz.fizzbuzz(15), 'fizzbuzz')
 
 
 if __name__ == '__main__':
